abf627d0aac042fde0e2043c9c51f1a0b230dd77
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-07 17:48:52 -0400

    added data validation function and added event listener to form which overrides default behavior and calls data validation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,50 @@
 // call load function at top of page
 
-// declare global variables
+// declare variables
+
+let form = document.querySelector('form')
+
+// add eventlistener and function to call when submit button is clicked
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    formValidator()
+})
+
+
 
 // function - data validation for form
+let inputId = document.querySelector('#inputId')
+let inputTitle = document.querySelector('#inputTitle')
+let inputDate = document.querySelector('#inputDate')
+let inputNote = document.querySelector('#inputNote')
+let validationMsg = document.querySelector('.validationMsg')
+
+let formValidator = () => {
+    if (inputId.value === '') {
+        validationMsg.innerHTML = '<p>ID Blank</p>'
+    }
+    else if (inputTitle.value === '') {
+        validationMsg.innerHTML = '<p>Title Blank</p>'
+    }
+    else if (inputDate.value === '') {
+        validationMsg.innerHTML = '<p>Date Blank</p>'
+    }
+    else if (inputNote.value === '') {
+        validationMsg.innerHTML = '<p>Note Blank</p>'
+    }
+    else {
+        validationMsg.innerHtml = ''
+        acceptData()
+    }
+}
 
 // function - write data to localstorage
+
+let acceptData = () => {
+    console.log('Data accepted')
+}
 
 // function - load data from localstorage and display on page
 
