5c136cfba1b4fe9976aef164fb3805aa73b0acc4
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-07 17:00:30 -0400

    add basic pseudocode for index.html and main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// call load function at top of page
+
+// declare global variables
+
+// function - data validation for form
+
+// function - write data to localstorage
+
+// function - load data from localstorage and display on page
+
+// function - for edit button, moves data to form, removes data from localstorage
+
+// function - for delete button, removes data from localstorage
+
+// function - clear form (reset)
