eecf50462c725b2f46eb4e5c3ad093ad0f8a7e96
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-05-09 12:16:35 -0400

    added json data parser from localstorage and function to display saved notes. Also added comments to both js and html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // call load function at top of page
 
-// declare variables
+// declare form variable
 
 let form = document.querySelector('form')
 
@@ -8,18 +8,19 @@ let form = document.querySelector('form')
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-
     formValidator()
 })
 
-
-
 // function - data validation for form
+
+    // declare form variables
 let inputId = document.querySelector('#inputId')
 let inputTitle = document.querySelector('#inputTitle')
 let inputDate = document.querySelector('#inputDate')
 let inputNote = document.querySelector('#inputNote')
 let validationMsg = document.querySelector('.validationMsg')
+
+    // function - rejects inputs if any of the fields are blank
 
 let formValidator = () => {
     if (inputId.value === '') {
@@ -41,12 +42,53 @@ let formValidator = () => {
 }
 
 // function - write data to localstorage
+let data = []
 
 let acceptData = () => {
-    console.log('Data accepted')
+    data = readData()
+    data.push({
+        'id': inputId.value,
+        'title': inputTitle.value,
+        'date': inputDate.value,
+        'note': inputNote.value
+    })
+    localStorage.setItem('data', JSON.stringify(data))
+    console.log('Data Accepted', data)
+    readData()
 }
 
 // function - load data from localstorage and display on page
+
+let readData = () => {
+    data = JSON.parse(localStorage.getItem('data'))
+    
+    // not sure if will/would have reason to keep this separate from placing updated data into the html, but going to keep the load/parsing separate from that for now
+
+    createNotes()
+}
+
+// function - load notes into html
+
+let displayedNotes = document.querySelector('.notesDiv')
+
+let createNotes = () => {
+    displayedNotes.innerHTML = '' // reset notes
+    data.forEach((note, i) => {
+        displayedNotes.innerHTML += `
+        <div id="">
+						<h3>${note.title}</h3>
+						<div>
+							<button>Edit Note</button>
+							<button>Delete Note</button>
+						</div>
+						<span>${note.date}</span>
+						<span>${note.id}</span>
+						<p>${note.note}</p>
+					</div>
+        `
+    })
+}
+
 
 // function - for edit button, moves data to form, removes data from localstorage
 
