c8df3897b30ca07cb2ac5217aa4270c1dd23e9f4
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-06-12 15:12:32 -0400

    added in router and controller for CRUD api calls

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import express from 'express'
+import 'dotenv/config'
+import { router } from "./routes/noteRoutes.js"
+
+const port = process.env.PORT|| 8000
+
+const app = express()
+
+app.use('/api/notes', router)
+
+app.listen(port, () => console.log(`Running on ${port}`))
