157cea8f715d2bafc4c54de17dbad8ff8f46c0c5
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-07-02 17:26:48 -0400

    organize file structure, added serving of  static html to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import 'dotenv/config'
 import { router } from './routes/noteRoutes.js'
 import { errorHandler } from './middleware/errorMiddleware.js'
 import { connectDB } from './config/db.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
 const port = process.env.PORT|| 8000
 
 connectDB()
@@ -12,6 +14,16 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/notes', router)
+
+// Serve frontend
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend')))
+    app.get('*', (req, res) =>res.sendFile(path.resolve(__dirname, '../', 'frontend', 'index.html')))
+}
 
 app.use(errorHandler)
 
