6cb0844f5db524b9ef39854dca1bf244a4902596
Theo Pacun <62911461+tpacun@users.noreply.github.com>
2022-08-19 09:47:59 -0400

    adding protection to note routes

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { getNotes, setNote, updateNote, deleteNote } from '../controllers/noteController.js'
+import { protect } from '../middleware/authorizationMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getNotes).post(setNote)
-router.route('/:id').put(updateNote).delete(deleteNote)
+router.route('/').get(protect, getNotes).post(protect, setNote)
+router.route('/:id').put(protect, updateNote).delete(protect, deleteNote)
 
 export {router}
